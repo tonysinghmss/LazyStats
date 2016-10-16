bf5e9b373624744be3b3288803ca7085b7027fe3
tonysinghmss <manishankarsngh007@gmail.com>
2016-10-16 19:28:41 +0530

    SignOut and Revoke access refactored.

--- app/src/main/java/com/tony/lazystats/SigninActivity.java
+++ app/src/main/java/com/tony/lazystats/SigninActivity.java
@@ -1,12 +1,12 @@
 package com.tony.lazystats;
 
-import android.accounts.AccountManager;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -14,12 +14,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
-import com.google.android.gms.auth.GoogleAuthUtil;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
-import com.google.android.gms.common.AccountPicker;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -28,11 +28,11 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
 public class SigninActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener,
-GoogleApiClient.ConnectionCallbacks{
+        GoogleApiClient.ConnectionCallbacks {
 
     private static final String TAG = SigninActivity.class.getSimpleName();
-    private static final int REQ_ACCPICK = 1;
-    private static final int RC_SIGN_IN = 2;
+    private static final int RC_SIGN_IN = 9001;
+
 
     private GoogleSignInOptions mGoogleSignInOptions;
     private GoogleApiClient mGoogleApiClient;
@@ -49,6 +49,11 @@ GoogleApiClient.ConnectionCallbacks{
 
     private SignInButton btnSignIn;
     private ProgressDialog mProgressDialog;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,8 +64,9 @@ GoogleApiClient.ConnectionCallbacks{
         mSignOutReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                signOutIfConnected();
-                Log.d(TAG, "Sign out complete.");
+                Log.d(TAG, "Sign Out broadcast received.");
+                //mGoogleApiClient.connect();
+                signOut();
             }
         };
         this.registerReceiver(mSignOutReceiver, mSignOutFilter);
@@ -70,32 +76,25 @@ GoogleApiClient.ConnectionCallbacks{
         mRevokeAccessReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                Log.d(TAG,"Revoke access");
+                Log.d(TAG, "Revoke access");
+                //mGoogleApiClient.connect();
                 revokeAccess();
                 Log.d(TAG, "Complete access revoked.");
             }
         };
         this.registerReceiver(mRevokeAccessReceiver, mRevokeAccessFilter);
-        // [START Sign out if connected.]
-        //signOutIfConnected();
-        // [END Sign out if connected.]
         setContentView(R.layout.activity_signin);
         btnSignIn = (SignInButton) findViewById(R.id.btn_sign_in);
 
         btnSignIn.setOnClickListener(this);
         // [START Configure sign in]
         // configure sign in options for google account
-        mGoogleSignInOptions= new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        mGoogleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
         // [END Configure sign in]
-        isAccntConnected= false;
-        // [START Build Google api client]
-        /*mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();*/
-        // [END Build Google api client]
+        isAccntConnected = false;
+        initGAC();
         btnSignIn.setSize(SignInButton.SIZE_STANDARD);
         btnSignIn.setScopes(mGoogleSignInOptions.getScopeArray());
 
@@ -104,38 +103,19 @@ GoogleApiClient.ConnectionCallbacks{
 
     }
 
-    /*@Override
-    protected void onResume(){
-        super.onResume();
-        this.registerReceiver(mSignOutReceiver, mSignOutFilter);
-        this.registerReceiver(mRevokeAccessReceiver, mRevokeAccessFilter);
-    }*/
-
-    /*@Override
-    protected void onPause(){
-        super.onPause();
-        this.unregisterReceiver(mSignOutReceiver);
-        this.unregisterReceiver(mRevokeAccessReceiver);
-    }*/
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         this.unregisterReceiver(mSignOutReceiver);
         this.unregisterReceiver(mRevokeAccessReceiver);
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d(TAG, "onConnectionFailed: " + connectionResult);
-    }
-
-    private void initGAC(){
-        /*mGoogleSignInOptions= new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .setAccountName(acntName)
-                .build();*/
+    private void initGAC() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .enableAutoManage(this, this)
+                //.enableAutoManage(this, this)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, mGoogleSignInOptions)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
                 //.setAccountName(acntName)
                 .build();
     }
@@ -143,13 +123,16 @@ GoogleApiClient.ConnectionCallbacks{
     @Override
     public void onStart() {
         super.onStart();
-        if(mGoogleApiClient!=null){
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        if (isAccntConnected) {
             OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
             if (opr.isDone()) {
                 //  If the user's cached credentials are valid, the OptionalPendingResult will be "done"
                 // and the GoogleSignInResult will be available instantly.
-                Log.d(TAG, "Got cached sign in");
+                Log.d(TAG, "Got cached credentials of user.");
                 GoogleSignInResult result = opr.get();
+                //mGoogleApiClient.connect();
                 handleSignInResult(result);
             } else {
                 // If the user has not previously signed in on this device or the sign-in has expired,
@@ -160,6 +143,7 @@ GoogleApiClient.ConnectionCallbacks{
                     @Override
                     public void onResult(GoogleSignInResult googleSignInResult) {
                         hideProgressDialog();
+                        //mGoogleApiClient.connect();
                         handleSignInResult(googleSignInResult);
                     }
                 });
@@ -180,10 +164,41 @@ GoogleApiClient.ConnectionCallbacks{
                 null, null, new String[]{GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE}, true, null, null, null, null
         ),REQ_ACCPICK);*/
         Log.d(TAG, "Sign in method called.");
-        initGAC();
+        //initGAC();
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+
+    private void signOut() {
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.clearDefaultAccountAndReconnect();
+            Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                    new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(Status status) {
+                            Log.d(TAG, "Sign Out using Google Api.");
+                            mGoogleApiClient.disconnect();
+                            isAccntConnected = false;
+                        }
+                    });
+        }
+    }
+
+    private void revokeAccess() {
+        if (isAccntConnected) {
+            //signOut();
+            Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
+                    new ResultCallback<Status>() {
+                        @Override
+                        public void onResult(Status status) {
+                            Log.d(TAG, "Revoke access using Google Api.");
+                            // Delete Server side data
+                            isAccntConnected = false;
+                        }
+                    });
+        }
+    }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -196,9 +211,10 @@ GoogleApiClient.ConnectionCallbacks{
                 startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         } else*/
-        if (requestCode == RC_SIGN_IN) {
+        if (requestCode == RC_SIGN_IN && resultCode == RESULT_OK) {
             Log.d(TAG, "Sign in request");
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            mGoogleApiClient.connect();
             handleSignInResult(result);
         }
     }
@@ -209,7 +225,7 @@ GoogleApiClient.ConnectionCallbacks{
             // Sign in successful. Show authenticated UI
             // Set the data in intent and send it to next activity
             GoogleSignInAccount acct = result.getSignInAccount();
-            Log.d(TAG, "Account Profile URL: "+acct.getPhotoUrl().toString());
+            //Log.d(TAG, "Account Profile URL: "+acct.getPhotoUrl().toString());
             String userId = acct.getId();
             String userDisplayName = acct.getDisplayName();
             String userPhotoUrl = acct.getPhotoUrl().toString();
@@ -222,8 +238,8 @@ GoogleApiClient.ConnectionCallbacks{
             editor.putString("USER_EMAIL", userEmail);
             editor.commit();
             //dataIntent.putExtra("USER_EMAIL",userEmail);
-            Intent dataIntent = new Intent(this, MasterActivity.class);
-            startActivity(dataIntent);
+            Intent activityIntent = new Intent(this, MasterActivity.class);
+            startActivity(activityIntent);
         }
     }
 
@@ -242,55 +258,21 @@ GoogleApiClient.ConnectionCallbacks{
         }
     }
 
-    private void signOutIfConnected() {
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-            //mEmail=null;
-            mGoogleApiClient.clearDefaultAccountAndReconnect();
-            Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
-                    new ResultCallback<Status>() {
-                        @Override
-                        public void onResult(Status status) {
-                            Log.d(TAG, "Sign Out using Google Api.");
-                            mGoogleApiClient.disconnect();
-                            isAccntConnected = false;
-                        }
-                    });
-        }
-    }
 
-    private void revokeAccess(){
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected() && isAccntConnected == true) {
-
-            Auth.GoogleSignInApi.revokeAccess(mGoogleApiClient).setResultCallback(
-                    new ResultCallback<Status>() {
-                        @Override
-                        public void onResult(Status status) {
-                            Log.d(TAG, "Revoke access using Google Api.");
-                            signOutIfConnected();
-                        }
-                    });
-        }
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed: " + connectionResult);
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        this.isAccntConnected = true;
+        isAccntConnected = true;
+        //System.out.println("Account Connection status." + isAccntConnected);
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
+        Log.d(TAG, "Connection Suspended.");
     }
 
-    /*@Override
-    public void onConnected(@Nullable Bundle bundle) {
-        if(!sp.contains("USER_ID_TOKEN")){
-            signOut();
-        }
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }*/
 }
